021651e67bb178e1b8ea58879da5564856a4f202
raember <raphael.emberger@hotmail.ch>
2020-01-31 18:26:06 +0100

    Added missing files. Updated gitignore

---
+++ spoofbot/util/har.py
@@ -0,0 +1,204 @@
+"""Module to clean and anonymize HAR logs"""
+import json
+import os
+import zlib
+from base64 import b64decode
+from io import BytesIO
+
+import brotli
+from requests import Request
+from requests.structures import CaseInsensitiveDict
+from urllib3 import HTTPResponse
+
+from spoofbot.adapter.common import MockHTTPResponse
+from spoofbot.util.common import dict_list_to_dict, dict_list_to_tuple_list, encode_form_data
+
+
+def clean_all_in(directory: str, backup_ext: str = '.bak'):
+    for filename in os.listdir(directory):
+        clean_har(os.path.join(directory, filename), backup_ext)
+
+
+def clean_har(har_path: str, backup_ext: str = '.bak'):
+    har_filename = os.path.basename(har_path)
+    ext = os.path.splitext(har_path)[-1]
+    if ext == backup_ext:
+        print(f"File is a backup file: '{har_filename}'")
+        return
+    if ext != '.har':
+        print(f"File is not a HAR file: '{har_filename}'")
+        return
+    backup_path = f"{har_path}{backup_ext}"
+    backup_filename = os.path.basename(backup_path)
+    if os.path.isfile(backup_path):
+        print(f"Backup file for '{har_filename}' already exists ('{backup_filename}'). Reprocessing.")
+        os.remove(har_path)
+        os.rename(backup_path, har_path)
+
+    with open(har_path, 'r', encoding='utf-8-sig') as fp:
+        har = json.load(fp)
+    har = anonymize_har(har)
+    os.rename(har_path, backup_path)
+    with open(har_path, 'w', encoding='utf-8') as fp:
+        json.dump(har, fp, indent=2)
+    print(f"Cleaned file: '{har_filename}' Backup: '{backup_filename}'")
+
+
+def anonymize_har(har: dict) -> dict:
+    log = har.get('log', {})
+    return {
+        'log': {
+            'version': log.get('version', '1.0'),
+            'creator': log.get('creator', {
+                'name': 'Unnamed',
+                'version': ''
+            }),
+            'entries': list(map(_anonymize_entry, log.get('entries', [])))
+        }
+    }
+
+
+def _anonymize_entry(entry: dict) -> dict:
+    request = entry.get('request', {})
+    response = entry.get('response', {})
+    data = {
+        'request': {
+            'method': request.get('method', ''),
+            'url': request.get('url', ''),
+            'httpVersion': request.get('httpVersion', ''),
+            'headers': list(filter(lambda h: h is not None, map(_anonymize_header, request.get('headers', [])))),
+            'queryString': request.get('queryString', []),
+            'cookies': list(map(_anonymize_cookie, request.get('cookies', []))),
+            'headersSize': request.get('headersSize', 0),
+            'bodySize': request.get('bodySize', 0),
+            'postData': request.get('postData', {}),
+        },
+        'response': {
+            'status': response.get('status', 0),
+            'statusText': response.get('statusText', ''),
+            'httpVersion': response.get('httpVersion', ''),
+            'headers': list(filter(lambda h: h is not None, map(_anonymize_header, response.get('headers', [])))),
+            'cookies': list(map(_anonymize_cookie, response.get('cookies', []))),
+            'content': response.get('content', {}),
+            'redirectURL': response.get('redirectURL', ''),
+            'headersSize': response.get('headersSize', 0),
+            'bodySize': response.get('bodySize', 0),
+        }
+    }
+    if 'postData' not in request:
+        del data['request']['postData']
+    return data
+
+
+def _anonymize_header(header: dict) -> dict:
+    if header['name'].startswith(':'):  # Provisional header. Thanks, Chrome
+        # noinspection PyTypeChecker
+        return None
+    return {
+        'name': header.get('name'),
+        'value': header['value']
+    }
+
+
+def _anonymize_cookie(cookie: dict) -> dict:
+    return {
+        'name': cookie['name'],
+        'value': cookie['value'],
+        'expires': cookie.get('expires', None),
+        'httpOnly': cookie.get('httpOnly', False),
+        'secure': cookie.get('secure', False),
+    }
+
+
+def request_from_entry(entry: dict, encode: bool = False) -> Request:
+    request_entry = entry['request']
+    params = {}
+    data = {}
+    if 'postData' in request_entry:
+        post_data = dict_list_to_tuple_list(request_entry['postData']['params'], case_insensitive=False)
+        if encode:
+            data = encode_form_data(post_data)
+        else:
+            data = '&'.join(map('='.join, post_data))
+    return Request(
+        method=request_entry['method'].upper(),
+        url=request_entry['url'],
+        headers=CaseInsensitiveDict(dict_list_to_dict(request_entry['headers'], case_insensitive=True)),
+        files=None,
+        data=data,
+        json=None,
+        params=params,
+        auth=None,
+        cookies=dict_list_to_dict(request_entry['cookies']),
+        hooks=None,
+    )
+
+
+def response_from_entry(entry: dict) -> HTTPResponse:
+    response_entry = entry['response']
+    headers = dict_list_to_tuple_list(response_entry['headers'])
+    headers_set = CaseInsensitiveDict(headers)
+    content = response_entry['content']
+    body = bytearray()
+    if 'text' in content:
+        data = content['text'].encode('utf8')
+        if 'Content-Encoding' in headers_set:
+            if headers_set['Content-Encoding'] == 'gzip':
+                compressor = zlib.compressobj(9, zlib.DEFLATED, zlib.MAX_WBITS | 16)
+                body = compressor.compress(data) + compressor.flush()
+            elif headers_set['Content-Encoding'] == 'br':
+                body = brotli.compress(data)
+            elif 'encoding' in content:
+                raise Exception()
+            else:
+                body = data
+        elif 'encoding' in content:
+            encoding = content['encoding']
+            if encoding == 'base64':
+                body = b64decode(data)
+            else:
+                raise Exception()
+        else:
+            body = data
+    # if 'text' in content:
+    #     text = content['text']
+    #     if 'encoding' in content:
+    #         encoding = content['encoding']
+    #         if encoding == 'base64':
+    #             body = base64.b64decode(text)
+    #         else:
+    #             body = text.decode(encoding)
+    #     else:
+    #         body = text.encode('utf-8')
+    # else:
+    #     body = b""
+
+    # if 'Set-Cookie' in headers_set:
+    #     altered_headers = []
+    #     if 'date' in headers_set:
+    #         date_offset = datetime.now() - datetime.strptime(headers_set['date'], '%a, %d %b %Y %H:%M:%S GMT')
+    #     else:
+    #         date_offset = timedelta()
+    #     date_format = '%a, %d-%b-%y %H:%M:%S GMT'
+    #     for k, v in headers:
+    #         if k.lower() == 'set-cookie':
+    #             cookie = str_to_dict(v)
+    #             if 'expires' in cookie:
+    #                 expiration_date = datetime.strptime(cookie['expires'], date_format)
+    #                 cookie['expires'] = (expiration_date + date_offset).strftime(date_format)
+    #             altered_headers.append((k, dict_to_str(cookie)))
+    #         else:
+    #             altered_headers.append((k, v))
+    #     headers = altered_headers
+
+    return HTTPResponse(
+        body=BytesIO(body),
+        headers=headers,
+        status=response_entry['status'],
+        preload_content=False,
+        original_response=MockHTTPResponse(headers)
+    )
+
+
+if __name__ == '__main__':
+    clean_all_in('test_data')
---
+++ spoofbot/util/http.py
@@ -0,0 +1,146 @@
+import re
+import sys
+from enum import Enum
+from typing import Optional, Tuple, Any, Generator
+
+from publicsuffix2 import get_sld
+from urllib3.util import Url
+
+
+def is_ip(string: str) -> bool:
+    return re.match(r'^(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)$', string) is not None
+
+
+def is_domain(string: str) -> bool:
+    return not is_ip(string)
+
+
+def opaque_origin(url: Url) -> str:
+    """https://html.spec.whatwg.org/multipage/origin.html#concept-origin-opaque
+    https://html.spec.whatwg.org/multipage/origin.html#ascii-serialisation-of-an-origin"""
+    return f"{url.scheme}://{url.hostname}"
+
+
+def tuple_origin(url: Url) -> Tuple[str, str, int, str]:
+    """https://html.spec.whatwg.org/multipage/origin.html#concept-origin-tuple"""
+    # noinspection PyTypeChecker
+    return url.scheme, url.hostname, url.port, None
+
+
+def are_same_origin(a: Url, b: Url) -> bool:
+    """https://html.spec.whatwg.org/multipage/origin.html#same-origin"""
+    if opaque_origin(a) == opaque_origin(b):
+        return True
+    if tuple_origin(a)[:3] == tuple_origin(b)[:3]:
+        return True
+    return False
+
+
+def are_same_origin_domain(a: Url, b: Url) -> bool:
+    """https://html.spec.whatwg.org/multipage/origin.html#same-origin-domain"""
+    if opaque_origin(a) == opaque_origin(b):
+        return True
+    a_tuple, b_tuple = tuple_origin(a), tuple_origin(b)
+    if a_tuple[0] == b_tuple[0] and a_tuple[1] is not None and b_tuple[1] is not None and a_tuple[1] == b_tuple[1]:
+        return True
+    return False
+
+
+# noinspection SpellCheckingInspection
+def are_schemelessly_same_site(a: Url, b: Url) -> bool:
+    """https://html.spec.whatwg.org/multipage/origin.html#schemelessly-same-site"""
+    if opaque_origin(a) == opaque_origin(b):
+        return True
+    host_a, host_b = a.hostname, b.hostname
+    reg_dom_a = get_sld(a.url)
+    if host_a == host_b and reg_dom_a is not None:
+        return True
+    if reg_dom_a is not None and reg_dom_a == get_sld(b.url):
+        return True
+    return False
+
+
+def are_same_site(a: Url, b: Url) -> bool:
+    """https://html.spec.whatwg.org/multipage/origin.html#same-site"""
+    return are_schemelessly_same_site(a, b) and (opaque_origin(a) == opaque_origin(b) or a.scheme == b.scheme)
+
+
+def strip_url_for_referrer(url: Url, origin_only: bool = False) -> str:
+    if url is None or url.scheme in ['about', 'blob', 'data']:
+        return 'no referer'
+    if origin_only:
+        return Url(url.scheme, host=url.hostname, port=url.port).url
+    return Url(url.scheme, host=url.hostname, port=url.port, path=url.path, query=url.query).url
+
+
+def is_downgrade(origin: Url, target: Url) -> bool:
+    return origin.scheme == 'https' and target.scheme != 'https'
+
+
+class ReferrerPolicy(Enum):
+    NO_REFERRER = 0
+    NO_REFERRER_WHEN_DOWNGRADE = 1
+    SAME_ORIGIN = 2
+    ORIGIN = 3
+    STRICT_ORIGIN = 4
+    ORIGIN_WHEN_CROSS_ORIGIN = 5
+    STRICT_ORIGIN_WHEN_CROSS_ORIGIN = 6
+    UNSAFE_URL = 7
+
+    def get_referrer(self, origin: Url, target: Url, origin_only: bool = False) -> Optional[str]:
+        if self == ReferrerPolicy.NO_REFERRER or origin is None:
+            return None
+        if self == ReferrerPolicy.NO_REFERRER_WHEN_DOWNGRADE:
+            if is_downgrade(origin, target):
+                return None
+            return strip_url_for_referrer(origin, origin_only)
+        if self == ReferrerPolicy.SAME_ORIGIN:
+            if are_same_origin(origin, target):
+                return strip_url_for_referrer(origin, origin_only)
+            return None
+        if self == ReferrerPolicy.ORIGIN:
+            return strip_url_for_referrer(origin, True)
+        if self == ReferrerPolicy.STRICT_ORIGIN:
+            if is_downgrade(origin, target):
+                return strip_url_for_referrer(origin, True)
+            return None
+        if self == ReferrerPolicy.ORIGIN_WHEN_CROSS_ORIGIN:
+            if are_same_origin(origin, target):
+                return strip_url_for_referrer(origin, origin_only)
+            return strip_url_for_referrer(origin, True)
+        if self == ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN:
+            if are_same_origin(origin, target):
+                return strip_url_for_referrer(origin, origin_only)
+            if is_downgrade(origin, target):
+                return strip_url_for_referrer(origin, True)
+            return None
+        if self == ReferrerPolicy.UNSAFE_URL:
+            return strip_url_for_referrer(origin, origin_only)
+        raise LookupError(f"Could not look determine referrer url with {self} policy.")
+
+    def get_origin(self, origin: Url, target: Url) -> Optional[str]:
+        if self == ReferrerPolicy.NO_REFERRER or origin is None:
+            return None
+        if self in [
+            ReferrerPolicy.NO_REFERRER_WHEN_DOWNGRADE,
+            ReferrerPolicy.STRICT_ORIGIN,
+            ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN
+        ]:
+            if is_downgrade(origin, target):
+                return None
+        if self == ReferrerPolicy.SAME_ORIGIN:
+            if not are_same_origin(origin, target):
+                return None
+        return strip_url_for_referrer(origin, True)
+
+
+def sort_dict(d: dict, precedence: list) -> Generator[Tuple[Any, Any], Any, None]:
+    items = []
+    for k, v in d.items():
+        try:
+            item_precedence = precedence.index(k)
+        except ValueError:
+            # no defined sort for this header, so we put it behind any other sorted header
+            item_precedence = sys.maxsize
+        items.append((item_precedence, k, v))
+    return ((k, v) for _, k, v in sorted(items))
