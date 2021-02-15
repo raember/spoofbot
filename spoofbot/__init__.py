6cdca2634f144c9745e591b3f42a66189b3206ee
raember <raember@users.noreply.github.com>
2021-02-15 10:20:17 +0100

    new fix

--- spoofbot/__init__.py
+++ spoofbot/__init__.py
@@ -5,4 +5,4 @@ from .browser import Browser, Firefox, Chrome, MimeTypeTag, LanguageTag
 # noinspection PyUnresolvedReferences
 from .operating_system import OS, Windows, WindowsVersion, MacOSX, MacOSXVersion, Linux, LinuxDerivatives
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
