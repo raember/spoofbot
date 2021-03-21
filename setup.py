11782a5962cd9eb71e1783bdaa3cebab7e05433e
raember <raember@users.noreply.github.com>
2021-03-21 14:58:41 +0100

    Updated to new version 1.0.0

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-import setuptools
+from setuptools import setup
 
 import spoofbot
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="spoofbot",
     version=spoofbot.__version__,
     author="raember",
-    author_email="",
+    author_email="raember@users.noreply.github.com",
     description="A python requests wrapper for spoofing common browser behaviour",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/raember/spoofbot",
-    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    packages=["spoofbot"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
